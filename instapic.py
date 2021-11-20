5247d3670241a75ab02a2965218cbc1ddb1c89d6
Yousaf K Hamza <31238282+yousafkhamza@users.noreply.github.com>
2021-11-20 19:41:50 +0530

    updated

--- instapic.py
+++ instapic.py
@@ -1,8 +1,13 @@
+# Date: 11/20/2021
+# Author: Yousaf K Hamza
+# Description: Instagram DP Downloader with URL
+
 import requests
 import json
 import base64
 import os
 
+# URL Spliting.
 insta_username= input('Instagram Profile_URL/UserName: ')
 if insta_username.startswith("http"):
     if insta_username.endswith("/"):
@@ -11,18 +16,24 @@ if insta_username.startswith("http"):
         insta_username=insta_username.split(".com/")[1].split("?")[0]
     else:
         insta_username=insta_username=insta_username.split(".com/")[1]
+# Downloading path creation
+if not os.path.exists("./dp"):
+    os.makedirs("./dp")
 
+# URL Imgbb API
 key_imgbb="ab6f158359d0a96a54476b63d3529d31"
 insta_url='https://www.instagram.com'
 response = requests.get(f"{insta_url}/{insta_username}/?__a=1")
 
+# Main 
 if response.ok:
     json_data = json.loads(response.text)
     profile_pic_url=json_data["graphql"]["user"]["profile_pic_url_hd"]
     r = requests.get(profile_pic_url, allow_redirects=True)
-    open(f"{insta_username}.jpeg", 'wb').write(r.content)
+    print("Image Downloaded under dp Direcotory!")
+    open(f"./dp/{insta_username}.jpeg", 'wb').write(r.content)
     print("Image URL Generating.....")
-    with open(f"{insta_username}.jpeg", "rb") as file:
+    with open(f"./dp/{insta_username}.jpeg", "rb") as file:
         url = "https://api.imgbb.com/1/upload"
         payload = {
             "key": key_imgbb,
@@ -30,7 +41,6 @@ if response.ok:
             }
         r = requests.post(url, data= payload)
         view_url=(json.loads(r.text)["data"]["url_viewer"])
-        os.remove(f"{insta_username}.jpeg")
         print("Image URL is: "+view_url)
 else:
-    print("Username/Profile URL isn't correct...")
+    print("UserName/Profile URL isn't correct...")
